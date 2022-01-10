c2a0860d7a3a95cd58e8617257b6372a14eadc88
Connor Glosser <glosser1@gmail.com>
2022-01-10 14:14:42 -0500

    Add pruning cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 use std::collections::{HashMap, HashSet};
 use std::io::Write;
+use std::str::FromStr;
+
+#[derive(Clone, Debug)]
+enum Command {
+    Guess(String),
+    Prune(char, usize),
+    PruneAll(char),
+}
+
+impl FromStr for Command {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (cmd, args) = s.split_once(' ').unwrap();
+        match cmd {
+            "guess" | "g" => Ok(Command::Guess(args.to_string())),
+            "prune" | "p" => {
+                let (ch, idx) = args.split_once(' ').unwrap();
+                Ok(Command::Prune(
+                    ch.chars().next().ok_or("Invalid character argument")?,
+                    idx.trim().parse()?,
+                ))
+            }
+            "pruneAll" | "pa" => Ok(Command::PruneAll(
+                args.chars().next().ok_or("Invalid character argument")?,
+            )),
+            _ => Err("Invalid command".into()),
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Tile {
@@ -23,66 +53,82 @@ fn main() {
     while words.len() > 1 {
         println!("Guesses: {:?}", guesses);
 
-        print!("Guess: ");
+        print!("$ ");
         std::io::stdout().flush().unwrap();
-        let mut guess = String::new();
-        std::io::stdin().read_line(&mut guess).unwrap();
-        guess = guess.trim().to_string();
+        let mut command_string = String::new();
+        std::io::stdin().read_line(&mut command_string).unwrap();
+        let command = Command::from_str(&command_string);
 
-        guesses.push(guess.clone());
-        let mut tiles: Vec<_> = guess.chars().map(Tile::Unchecked).collect();
+        if let Err(_) = command {
+            continue;
+        }
 
-        input_digits("  Correct placement: ")
-            .into_iter()
-            .for_each(|i| match tiles[i] {
-                Tile::Unchecked(c) => tiles[i] = Tile::Correct(c),
-                _ => unreachable!(),
-            });
-
-        input_digits("Incorrect placement: ")
-            .into_iter()
-            .for_each(|i| match tiles[i] {
-                Tile::Unchecked(c) => tiles[i] = Tile::Incorrect(c),
-                _ => unreachable!(),
-            });
-
-        for tile in tiles.iter_mut() {
-            if let Tile::Unchecked(c) = *tile {
-                *tile = Tile::Unused(c);
+        match command.unwrap() {
+            Command::PruneAll(ch) => {
+                words.retain(|word| !word.contains(ch));
             }
-        }
+            Command::Prune(ch, idx) => {
+                words.retain(|word| word.chars().nth(idx).unwrap() != ch);
+            }
+            Command::Guess(arg) => {
+                let guess = arg.trim().to_string();
 
-        if tiles.iter().all(|t| matches!(t, Tile::Unused(_))) {
-            words.retain(|word| {
-                !tiles.iter().any(|t| -> bool {
-                    match t {
-                        Tile::Unused(c) => word.contains(*c),
-                        _ => false,
+                guesses.push(guess.clone());
+                let mut tiles: Vec<_> = guess.chars().map(Tile::Unchecked).collect();
+
+                input_digits("  Correct placement: ")
+                    .into_iter()
+                    .for_each(|i| match tiles[i] {
+                        Tile::Unchecked(c) => tiles[i] = Tile::Correct(c),
+                        _ => unreachable!(),
+                    });
+
+                input_digits("Incorrect placement: ")
+                    .into_iter()
+                    .for_each(|i| match tiles[i] {
+                        Tile::Unchecked(c) => tiles[i] = Tile::Incorrect(c),
+                        _ => unreachable!(),
+                    });
+
+                for tile in tiles.iter_mut() {
+                    if let Tile::Unchecked(c) = *tile {
+                        *tile = Tile::Unused(c);
                     }
-                })
-            })
-        }
+                }
 
-        for (i, tile) in tiles.iter().enumerate() {
-            words.retain(|word| {
-                let letter = word.chars().nth(i).unwrap();
-                match *tile {
-                    Tile::Correct(c) => letter == c,
-                    Tile::Incorrect(c) => tiles
-                        .iter()
-                        .zip(word.chars())
-                        .filter_map(|(&tile, char)| -> Option<bool> {
-                            match tile {
-                                Tile::Unused(_) => Some(c == char),
-                                Tile::Incorrect(d) => Some(c != d && c == char),
-                                _ => None,
+                if tiles.iter().all(|t| matches!(t, Tile::Unused(_))) {
+                    words.retain(|word| {
+                        !tiles.iter().any(|t| -> bool {
+                            match t {
+                                Tile::Unused(c) => word.contains(*c),
+                                _ => false,
                             }
                         })
-                        .any(|b| b),
-                    Tile::Unused(c) => letter != c,
-                    Tile::Unchecked(_) => unreachable!(),
+                    })
                 }
-            });
+
+                for (i, tile) in tiles.iter().enumerate() {
+                    words.retain(|word| {
+                        let letter = word.chars().nth(i).unwrap();
+                        match *tile {
+                            Tile::Correct(c) => letter == c,
+                            Tile::Incorrect(c) => tiles
+                                .iter()
+                                .zip(word.chars())
+                                .filter_map(|(&tile, char)| -> Option<bool> {
+                                    match tile {
+                                        Tile::Unused(_) => Some(c == char),
+                                        Tile::Incorrect(d) => Some(c != d && c == char),
+                                        _ => None,
+                                    }
+                                })
+                                .any(|b| b),
+                            Tile::Unused(c) => letter != c,
+                            Tile::Unchecked(_) => unreachable!(),
+                        }
+                    });
+                }
+            }
         }
 
         println!("{:?}", words);
